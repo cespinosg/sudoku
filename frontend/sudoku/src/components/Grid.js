2d5a5041c79cedcef5322e9c9355ec58420e600d
AntonBenet <antonbenetg@gmail.com>
2021-01-31 00:56:29 +0100

    Footer is now fixed to bottom. Editable sudoku grid created.

---
+++ frontend/sudoku/src/components/Grid.js
@@ -0,0 +1,113 @@
+import React, { Component } from 'react';
+
+class Grid extends Component { 
+    render() {
+        return (
+            <div className="grid">
+             <table className="sudokuTable">
+             <tr>
+    <td contentEditable='true' min="1" max="9"></td>
+    <td contentEditable='true' min="1" max="9"></td>
+    <td contentEditable='true' min="1" max="9"></td>
+    <td contentEditable='true' min="1" max="9"></td>
+    <td contentEditable='true' min="1" max="9"></td>
+    <td contentEditable='true' min="1" max="9"></td>
+    <td contentEditable='true' min="1" max="9"></td>
+    <td contentEditable='true' min="1" max="9"></td>
+    <td contentEditable='true' min="1" max="9"></td>
+  </tr>
+  <tr>
+  <td contentEditable='true' min="1" max="9"></td>
+  <td contentEditable='true' min="1" max="9"></td>
+  <td contentEditable='true' min="1" max="9"></td>
+  <td contentEditable='true' min="1" max="9"></td>
+  <td contentEditable='true' min="1" max="9"></td>
+  <td contentEditable='true' min="1" max="9"></td>
+  <td contentEditable='true' min="1" max="9"></td>
+  <td contentEditable='true' min="1" max="9"></td>
+  <td contentEditable='true' min="1" max="9"></td>
+  </tr>
+  <tr>
+  <td contentEditable='true' min="1" max="9"></td>
+  <td contentEditable='true' min="1" max="9"></td>
+  <td contentEditable='true' min="1" max="9"></td>
+  <td contentEditable='true' min="1" max="9"></td>
+  <td contentEditable='true' min="1" max="9"></td>
+  <td contentEditable='true' min="1" max="9"></td>
+  <td contentEditable='true' min="1" max="9"></td>
+  <td contentEditable='true' min="1" max="9"></td>
+  <td contentEditable='true' min="1" max="9"></td>
+  </tr>
+  <tr>
+  <td contentEditable='true' min="1" max="9"></td>
+  <td contentEditable='true' min="1" max="9"></td>
+  <td contentEditable='true' min="1" max="9"></td>
+  <td contentEditable='true' min="1" max="9"></td>
+  <td contentEditable='true' min="1" max="9"></td>
+  <td contentEditable='true' min="1" max="9"></td>
+  <td contentEditable='true' min="1" max="9"></td>
+  <td contentEditable='true' min="1" max="9"></td>
+  <td contentEditable='true' min="1" max="9"></td>
+  </tr>
+  <tr>
+  <td contentEditable='true' min="1" max="9"></td>
+  <td contentEditable='true' min="1" max="9"></td>
+  <td contentEditable='true' min="1" max="9"></td>
+  <td contentEditable='true' min="1" max="9"></td>
+  <td contentEditable='true' min="1" max="9"></td>
+  <td contentEditable='true' min="1" max="9"></td>
+  <td contentEditable='true' min="1" max="9"></td>
+  <td contentEditable='true' min="1" max="9"></td>
+  <td contentEditable='true' min="1" max="9"></td>
+  </tr>
+  <tr>
+  <td contentEditable='true' min="1" max="9"></td>
+  <td contentEditable='true' min="1" max="9"></td>
+  <td contentEditable='true' min="1" max="9"></td>
+  <td contentEditable='true' min="1" max="9"></td>
+  <td contentEditable='true' min="1" max="9"></td>
+  <td contentEditable='true' min="1" max="9"></td>
+  <td contentEditable='true' min="1" max="9"></td>
+  <td contentEditable='true' min="1" max="9"></td>
+  <td contentEditable='true' min="1" max="9"></td>
+  </tr>
+  <tr>
+  <td contentEditable='true' min="1" max="9"></td>
+  <td contentEditable='true' min="1" max="9"></td>
+  <td contentEditable='true' min="1" max="9"></td>
+  <td contentEditable='true' min="1" max="9"></td>
+  <td contentEditable='true' min="1" max="9"></td>
+  <td contentEditable='true' min="1" max="9"></td>
+  <td contentEditable='true' min="1" max="9"></td>
+  <td contentEditable='true' min="1" max="9"></td>
+  <td contentEditable='true' min="1" max="9"></td>
+  </tr>
+  <tr>
+  <td contentEditable='true' min="1" max="9"></td>
+  <td contentEditable='true' min="1" max="9"></td>
+  <td contentEditable='true' min="1" max="9"></td>
+  <td contentEditable='true' min="1" max="9"></td>
+  <td contentEditable='true' min="1" max="9"></td>
+  <td contentEditable='true' min="1" max="9"></td>
+  <td contentEditable='true' min="1" max="9"></td>
+  <td contentEditable='true' min="1" max="9"></td>
+  <td contentEditable='true' min="1" max="9"></td>
+  </tr>
+  <tr>
+  <td contentEditable='true' min="1" max="9"></td>
+  <td contentEditable='true' min="1" max="9"></td>
+  <td contentEditable='true' min="1" max="9"></td>
+  <td contentEditable='true' min="1" max="9"></td>
+  <td contentEditable='true' min="1" max="9"></td>
+  <td contentEditable='true' min="1" max="9"></td>
+  <td contentEditable='true' min="1" max="9"></td>
+  <td contentEditable='true' min="1" max="9"></td>
+  <td contentEditable='true' min="1" max="9"></td>
+  </tr>
+      </table>   
+            </div>
+        )
+    }
+}
+
+export default Grid;
