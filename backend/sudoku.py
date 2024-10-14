495ab3e3c763c57ec004a494f32e7ddab9e59bda
Carlos <cespinos@outlook.es>
2024-10-14 20:02:23 +0200

    Added a function for trimming the group options based on its subgroups.

--- backend/sudoku.py
+++ backend/sudoku.py
@@ -15,7 +15,7 @@ class Sudoku:
         self.sdm = line
         self._build()
         self._set_groups()
-        self.set_options()
+        self._set_options()
 
     def _build(self):
         '''
@@ -25,6 +25,7 @@ class Sudoku:
         array = [numbers[9*i:9*(i+1)] for i in range(9)]
         self.array = np.array(array)
         self.cells = np.array([[Cell(ri) for ri in r] for r in self.array])
+        self.n_solved_cells = sum([ni > 0 for ni in numbers])
 
     def _set_groups(self):
         '''
@@ -39,6 +40,22 @@ class Sudoku:
             cells = self.cells[3*i:3*(i+1), 3*j:3*(j+1)].flatten()
             self.squares[k] = Group(cells)
 
+    def _set_options(self):
+        '''
+        Sets the cell options.
+        '''
+        for i in range(9):
+            self.rows[i].update_options()
+            self.cols[i].update_options()
+            self.squares[i].update_options()
+        self._set_n_options()
+
+    def _set_n_options(self):
+        '''
+        Sets the number of options.
+        '''
+        self.n_options = sum([len(c.options) for c in self.cells.flatten()])
+
     def __repr__(self):
         '''
         Creates the sudoku string representation.
@@ -51,35 +68,73 @@ class Sudoku:
         lns = lns+hrule
         return '\n'.join(lns).replace('0', ' ')
 
-    def set_options(self):
+    def is_solved(self):
         '''
-        Sets the cell options.
+        Checks if the sudoku is solved.
         '''
-        for i in range(9):
-            self.rows[i].update_options()
-            self.cols[i].update_options()
-            self.squares[i].update_options()
+        rows = all([r.is_solved() for r in self.rows])
+        cols = all([c.is_solved() for c in self.cols])
+        squares = all([s.is_solved() for s in self.squares])
+        return all([rows, cols, squares])
 
     def solve(self):
         '''
         Solves the sudoku.
         '''
+        n_solved_cells_old = 0
+        n_options_old = self.n_options
+        self.iterations = 0
+        while any([n_solved_cells_old < self.n_solved_cells,
+            self.n_options < n_options_old]):
+            print(f'\nIteration {self.iterations}')
+            n_solved_cells_old = self.n_solved_cells
+            n_options_old = self.n_options
+            self.iterate()
+            self.iterations += 1
+
+    def iterate(self):
+        '''
+        Does one iteration to solve the sudoku.
+        '''
+        print('Before passing')
+        print(f'  Solved cells: {self.n_solved_cells}')
+        print(f'  Options: {self.n_options}')
         for i in range(9):
             for j in range(9):
-                k = int(i/3)*3+int(j/3)
+                self._trim_groups_options(i, j)
                 if len(self.cells[i, j].options) == 1:
-                    self.solve_cell(i, j, self.cells[i, j].options[0])
-                only_option_in_row = self.rows[i].check_cell(self.cells[i, j])
-                only_option_in_col = self.cols[j].check_cell(self.cells[i, j])
-                only_option_in_s = self.squares[k].check_cell(self.cells[i, j])
-                if only_option_in_row is not None:
-                    self.solve_cell(i, j, only_option_in_row)
-                elif only_option_in_col is not None:
-                    self.solve_cell(i, j, only_option_in_col)
-                elif only_option_in_s is not None:
-                    self.solve_cell(i, j, only_option_in_s)
+                    self._solve_cell(i, j, self.cells[i, j].options[0])
+                self._check_unique_option(i, j)
+        self._set_n_options()
+        print('After passing')
+        print(f'  Solved cells: {self.n_solved_cells}')
+        print(f'  Options: {self.n_options}')
 
-    def solve_cell(self, i, j, value):
+    def _trim_groups_options(self, i, j):
+        '''
+        Trims the groups options.
+        '''
+        k = int(i/3)*3+int(j/3)
+        self.rows[i].trim_options()
+        self.cols[j].trim_options()
+        self.squares[k].trim_options()
+
+    def _check_unique_option(self, i, j):
+        '''
+        Checks if the given cell has a unique option among its groups.
+        '''
+        k = int(i/3)*3+int(j/3)
+        only_option_in_row = self.rows[i].check_cell(self.cells[i, j])
+        only_option_in_col = self.cols[j].check_cell(self.cells[i, j])
+        only_option_in_s = self.squares[k].check_cell(self.cells[i, j])
+        if only_option_in_row is not None:
+            self._solve_cell(i, j, only_option_in_row)
+        elif only_option_in_col is not None:
+            self._solve_cell(i, j, only_option_in_col)
+        elif only_option_in_s is not None:
+            self._solve_cell(i, j, only_option_in_s)
+
+    def _solve_cell(self, i, j, value):
         '''
         Sets the given value to the given cell.
         '''
@@ -88,6 +143,7 @@ class Sudoku:
         self.cols[j].update()
         k = int(i/3)*3+int(j/3)
         self.squares[k].update()
+        self.n_solved_cells += 1
 
 
 class Cell:
@@ -127,7 +183,7 @@ class Group:
         Sets the values of the group.
         '''
         self.values = sorted([c.value for c in self.cells if c.value != 0])
-        self.missing = [i for i in range(1, 10) if i not in self.values]
+        self.unsolved_ids = [i for i in range(9) if self.cells[i].value == 0]
 
     def update_options(self):
         '''
@@ -142,6 +198,12 @@ class Group:
         self.set_values()
         self.update_options()
 
+    def is_solved(self):
+        '''
+        Checks if the group is solved.
+        '''
+        return len(set(self.values)) == 9
+
     def check_cell(self, cell):
         '''
         Looks if the given cell has missing values that are only feasible there.
@@ -154,6 +216,34 @@ class Group:
             return value[0]
         else:
             return None
+
+    def trim_options(self):
+        '''
+        Checks the options in the group and removes them if a subgroup has a
+        closed set of options.
+        
+        For example, if two cells only can be 1 or 2, these options
+        can be removed from the rest of cells.
+        '''
+        self.find_closed_subgroups(2)
+        self.find_closed_subgroups(3)
+
+    def find_closed_subgroups(self, n):
+        '''
+        Finds subgroups with a closed set of options with the given size.
+        '''
+        for subgroup_indices in itertools.combinations(self.unsolved_ids, n):
+            options = self.get_subgroup_options(subgroup_indices)
+            if len(options) == len(subgroup_indices):
+                for k in self.unsolved_ids:
+                    if k not in subgroup_indices:
+                        self.cells[k].remove_options(options)
+
+    def get_subgroup_options(self, indices):
+        '''
+        Returns the options of the given subgroup.
+        '''
+        return sorted(set([o for i in indices for o in self.cells[i].options]))
 
 
 if __name__ == '__main__':
@@ -202,7 +292,7 @@ if __name__ == '__main__':
               '000064000',
              ]
     # source for sudokus: https://sudoku.com/es/normal/
-    ln = ''.join(hard)
+    ln = ''.join(expert)
     print(f'\ninput: \'{ln}\'\n')
     sudoku = Sudoku(ln)
     print(f'sudoku:\n{sudoku}\n')
